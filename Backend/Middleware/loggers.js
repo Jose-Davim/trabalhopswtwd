e1b68f64cc9020e48059767b315c3a24eb82836a
ruby2102 <diogo3002.spam@gmail.com>
2025-01-10 11:14:11 +0000

    teste auth

--- Backend/Middleware/loggers.js
+++ Backend/Middleware/loggers.js
@@ -1,5 +1,19 @@
-module.exports = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-   };
-   
+import Users from "../Models/users";
+
+export const CheckRole = (allowedRoles) => {
+    return async (req, res, next) => {
+        const userId = req.userId;
+        const userInfo = await Users.findById(userId, {
+            role: true
+        });
+
+        if(!allowedRoles.includes(userInfo.role)){
+            return res.status(401).json({
+                "message": "Impossivel aceder a este Endpoint!",
+                "code": 0
+            });
+        }
+
+        next();
+    };
+};
